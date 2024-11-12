f877c9a1935b1305d8e6a4c30abbc08bad0206dc
ogisetiawan <ogisetiawan21@gmail.com>
2024-11-12 20:53:59 +0700

    ✅ add: AI workflow module

--- module/Modules - Noted.php
+++ module/Modules - Noted.php
@@ -2,6 +2,35 @@
 /*
  * @Author: ogi.setiawan 
  * @Date: 2024-10-30 11:00:14 
- * @Last Modified by:   ogi.setiawan 
- * @Last Modified time: 2024-10-30 11:00:14 
+ * @Last Modified by: ogi.setiawan
+ * @Last Modified time: 2024-10-30 13:00:26
  */
+
+//@ Berkenalan dengan Artificial Intelligence (AI)
+//~ Taksonomi AI
+//? AI: Kecerdasan Buatan adalah teknologi yang memungkinkan komputer meniru perilaku manusia, seperti belajar, mengambil keputusan, dan menyelesaikan masalah.
+//? Machine Learning (ML): Merupakan bagian dari AI yang memungkinkan komputer belajar dari data tanpa diprogram secara eksplisit. ML menggunakan algoritma untuk menemukan pola dalam data dan membuat prediksi.
+//? Deep Learning: Adalah subbidang dari ML yang menggunakan jaringan saraf tiruan untuk mempelajari representasi data yang kompleks. Deep Learning sangat efektif dalam menangani data yang tidak terstruktur, seperti gambar dan teks.
+//? Generative AI: Merupakan jenis Deep Learning yang dapat menghasilkan konten baru, seperti gambar, teks, atau musik, berdasarkan data yang telah dipelajarinya
+/// Example
+//? Sistem rekomendasi pada platform streaming, deteksi spam dalam email. yang dibuat oleh ML
+//? Deep Learning: Pengenalan wajah, terjemahan bahasa, mobil self-driving.
+//? Generative AI: Pembuatan gambar realistis dari teks deskriptif (DALL-E), pembuatan musik.
+
+//~ AI workflow (alur kerja ai)
+//? Alur pengembangan AI merupakan serangkaian langkah yang harus dilalui untuk menciptakan sebuah sistem kecerdasan buatan. Mari kita ringkas setiap tahapannya:
+/// Digitalisasi dan Pengumpulan Data: (Digitalise & Collect)
+//? Tujuan: Mengubah data dari format analog (misal, kertas) menjadi format digital yang bisa diolah komputer.
+//? Langkah: Mengumpulkan data dari berbagai sumber, baik data terstruktur (tabel, database) maupun tidak terstruktur (teks, gambar, video).
+/// Transformasi Data: 
+//? Tujuan: Menyiapkan data agar siap diolah oleh model AI.
+//? Langkah: Membersihkan data, mengubah format, dan mengekstrak fitur-fitur penting dari data.
+/// Pelatihan Model:
+//? Tujuan: Mengajarkan model AI untuk mengenali pola dan membuat prediksi berdasarkan data yang telah dilatih.
+//? Langkah: Memilih algoritma yang sesuai, melatih model dengan data yang telah ditransformasi, dan mengevaluasi kinerja model.
+/// Eksekusi Model:
+//? Tujuan: Menggunakan model AI yang telah dilatih untuk menyelesaikan tugas-tugas tertentu.
+//? Langkah: Menerapkan model pada data baru dan menganalisis hasil prediksi.
+/// Analisis dan Pengambilan Keputusan:
+//? Tujuan: Memahami hasil yang dihasilkan oleh model AI dan mengambil tindakan berdasarkan pemahaman tersebut.
+//? Langkah: Mengekstrak informasi berharga dari model, membuat keputusan bisnis, dan meningkatkan model secara terus-menerus.
